4a716186d826245466e7549b5f9928dcf5c8033f
Taylor Huffman <taylor@crucialartists.com>
2022-08-18 14:59:16 -0500

    Complete assignment - change argument in signups

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -12,7 +12,7 @@ class SignupsController < ApplicationController
         params.permit(:camper_id, :activity_id, :time)
     end
 
-    def render_unprocessable_entity_response(exception)
-        render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
       end
 end
